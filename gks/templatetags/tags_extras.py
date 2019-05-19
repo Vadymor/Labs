bc7992d98552572fcf291c4c766ed643f0a7c926
Vadymor <Mona>
2019-05-20 00:14:51 +0300

    create gant diagram

--- gks/templatetags/tags_extras.py
+++ gks/templatetags/tags_extras.py
@@ -1,4 +1,5 @@
 from django import template
+import random
 
 register = template.Library()
 
@@ -6,3 +7,24 @@ register = template.Library()
 @register.filter(name='position')
 def position(pos):
     return 45 + 40 * pos
+
+
+@register.filter(name='y_pos')
+def y_pos(pos):
+    return 140 + 40 * pos
+
+
+@register.filter(name='x_pos')
+def x_pos(pos):
+    return 50 + 40 * pos[0]
+
+
+@register.filter(name='length')
+def length(pos):
+    return 40 * (pos[1] - pos[0])
+
+
+@register.filter(name='color')
+def color(num):
+    color = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
+    return color
